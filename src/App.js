e6bfd36bb049f7a6c9c16834527b6fbdc26b2bcd
CathyAkoth <catherinececilia22@gmail.com>
2021-11-25 02:19:53 -0800

    More to Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
-
+import React from 'react';
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
-    <div> hello </div>
+    <Router> 
+      <Navbar />
+    </Router>
   );
 }
 
