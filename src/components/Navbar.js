8ff87916dd865685dfaf1fdb788e4830c1d50f35
CathyAkoth <catherinececilia22@gmail.com>
2021-11-25 01:15:34 -0800

    Addedddd

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+ import React,{useState} from 'react';
+ import {Button} from './Button';
+ import {Link} from 'react-router-dom';
+ import './Navbar.css';
+ import Dropdown from './Dropdown';
+ 
+ function Navbar(){
+     return(
+         <>
+         <nav className='navbar'>
+             <Link to='/' className='navbar-logo'>
+                 EPIC
+             </Link>
+         </nav>
+         </>
+     )
+ }
