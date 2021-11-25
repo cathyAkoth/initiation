7776ca49395000d7690f8e1e172846d8870563f8
CathyAkoth <catherinececilia22@gmail.com>
2021-11-25 08:34:48 -0800

    Added Font Awesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,18 @@
  import Dropdown from './Dropdown';
  
  function Navbar(){
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
      return(
          <>
          <nav className='navbar'>
              <Link to='/' className='navbar-logo'>
                  EPIC
              </Link>
+             <div className='menu-icon' onClick={handleClick}>
+                 <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+             </div>
          </nav>
          </>
      )
