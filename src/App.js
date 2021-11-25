e95ff361d7aea2a4d224be98b2e3522c4b03340f
CathyAkoth <catherinececilia22@gmail.com>
2021-11-25 09:16:00 -0800

    Added Nav-Items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import  './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,10 @@
  
  function Navbar(){
 
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const [dropdown, setDropdpwn] = useState(false);
+    const closeMobileMenu = () => setClick(false);
      return(
          <>
          <nav className='navbar'>
@@ -17,6 +19,30 @@
              <div className='menu-icon' onClick={handleClick}>
                  <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
              </div>
+             <ul className={click ? 'nav-menu active' : 'nav-menu'}> 
+             <li className='nav-item'>
+                 <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                   Home  
+                 </Link>
+             </li>
+             <li className='nav-item'>
+                 <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                    services <i className='fas fa-caret-down' /> 
+                 </Link>
+                 {dropdown && <Dropdown />}
+             </li>
+             <li className='nav-item'>
+                 <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                   Contact Us  
+                 </Link>
+             </li>
+             <li className='nav-item'>
+                 <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                   Sign Up  
+                 </Link>
+             </li>
+             </ul>
+             <Button />
          </nav>
          </>
      )
